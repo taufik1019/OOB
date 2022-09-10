3331ee31afeee6079809139c5fd01086ef0367f7
Taufik Andri Rahman <taufikandrirahman17@gmail.com>
2022-09-10 16:19:11 +0700

    initial commit

---
+++ TaufikAndriRahman_FundProg/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TaufikAndriRahman_FundProg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
